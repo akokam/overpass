b0615ad3550bfed6f3f83c96e6f44f006d79118d
Jakob Kammerer <jakob.kammerer@gmail.com>
2022-10-15 12:36:34 +0200

    Add specific errors and prefix config attributes

--- overpass/client.py
+++ overpass/client.py
@@ -4,7 +4,39 @@ from pydantic import BaseModel, HttpUrl, ValidationError
 from overpass.models import Response
 
 
+class OverpassError(Exception):
+    """Base error for overpass"""
+
+
+class RequestError(OverpassError):
+    """Base error for overpass requests"""
+
+
+class LocalConnectionError(OverpassError):
+    """Raised when the connection to overpass server failed"""
+
+
+class ServerError(RequestError):
+    """Raised when an overpass request failed on server side"""
+
+
+class ClientError(RequestError):
+    """Raised when an overpass request failed on client side"""
+
+
+class ParsingError(RequestError):
+    """Raised when an overpass response was not parsable"""
+
+
 class Config(BaseModel):
+    """Configuration for overpass
+
+    All environment variables shall be prefixed with `OVERPASS_`, e.g.:
+      - OVERPASS_BASE_URL=localhost:"""
+
+    class Config:
+        env_prefix = "OVERPASS_"
+
     base_url: HttpUrl = "https://lz4.overpass-api.de"
     port: int = 80
     timeout: int = 60
@@ -20,6 +52,13 @@ class Client:
         client_cfg = cfg or Config()
         self._overpass = httpx.Client(base_url=client_cfg.base_url)
 
+    def query(self, query: str) -> Response:
+        """Query the OpenStreetMap database with Overpass QL"""
+
+        # TODO: Add sanity checks for query
+
+        return self._post(query)
+
     def _post(self, payload: str) -> Response:
         """Perform a POST request to the interpreter
 
@@ -29,22 +68,17 @@ class Client:
         try:
             response = self._overpass.post(url="/api/interpreter", data=payload)
         except (TimeoutError, ConnectionError) as e:
-            # TODO: Raise specific error
-            raise Exception(e)
+            # TODO: Check if httpx.RequestError works too
+            raise LocalConnectionError(e)
 
         try:
             response.raise_for_status()
         except httpx.HTTPError as e:
             # TODO: Raise specific error
+            # TODO: Differentiate between client and server error
             raise Exception(e)
 
         try:
             return Response(**response.json())
         except ValidationError as e:
-            # TODO: Raise specific error
-            raise Exception(e)
-
-
-c = Client()
-
-c._post("")
+            raise ParsingError(e)
